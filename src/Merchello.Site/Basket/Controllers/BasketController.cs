23a3f5ec9c877998011543b3de716353116d8105
Lee Messenger <lee@aptitude.co.uk>
2025-12-23 13:39:40 +0000

    Starting to build out front end

---
+++ src/Merchello.Site/Basket/Controllers/BasketController.cs
@@ -0,0 +1,28 @@
+﻿using Merchello.Core.Shared.Models;
+using Merchello.Site.Shared.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+
+namespace Merchello.Site.Basket.Controllers;
+
+public class BasketController(IOptions<MerchelloSettings> options,
+    IUmbracoContextAccessor umbracoContextAccessor,
+    IUmbracoDatabaseFactory databaseFactory,
+    ServiceContext services,
+    AppCaches appCaches,
+    IProfilingLogger profilingLogger,
+    IPublishedUrlProvider publishedUrlProvider)
+    : BaseController(options, umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger,
+        publishedUrlProvider)
+{
+    public IActionResult Basket(Umbraco.Cms.Web.Common.PublishedModels.Basket model)
+    {
+        return CurrentTemplate(model);
+    }
+}
---
+++ src/Merchello.Site/Category/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using Merchello.Core.Shared.Models;
+using Merchello.Site.Shared.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+
+namespace Merchello.Site.Category.Controllers;
+
+public class CategoryController(
+    IOptions<MerchelloSettings> options,
+    IUmbracoContextAccessor umbracoContextAccessor,
+    IUmbracoDatabaseFactory databaseFactory,
+    ServiceContext services,
+    AppCaches appCaches,
+    IProfilingLogger profilingLogger,
+    IPublishedUrlProvider publishedUrlProvider)
+    : BaseController(options, umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger,
+        publishedUrlProvider)
+{
+    public IActionResult Category(Umbraco.Cms.Web.Common.PublishedModels.Category model)
+    {
+        return CurrentTemplate(model);
+    }
+}
